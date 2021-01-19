c87fced9c5fb9fd8e5059eb721427064da59059b
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-01-19 11:57:36 +0100

    lektion-1

--- Lektion-1/4. datatyper/script.js
+++ Lektion-1/4. datatyper/script.js
@@ -31,5 +31,76 @@
       let name = 'Joakim;
       let age = 34;
 
+    variabel = nått som vi lagrar information i
+      camelCase   =   javascript, Java, C, c++
+      Pascal      =   C#, Pascal
+      _private    =   en varibel som "bara" lever inom en viss del.
+
+      ex. firstName, lastName, email, city
+
+      _firstName = 'Joakim'
+      firstName = _firstName
 
 */
+
+
+/* 
+    STRINGS:
+      let name = "Joakim"   - Använd inte
+      let name = 'Joakim'   - Denna ska du använda
+      let name = `Joakim`   - Template String - Använd denna när du ska använda placeholders ( shift + ´ , tryck mellanslag)
+
+      avsluta varje rad med ett ; (det behövs inte, men det är bra att göra.)
+*/
+
+let doNotUse = "Joakim";
+let useThis = 'Joakim';
+let number = 34;
+
+// console.log(useThis, number);
+
+let withoutPlaceholders = 'Mitt namn är ' + useThis + ' och jag är ' + number + ' år gammal.'
+
+// Template string
+let withPlaceholders = `Mitt namn är ${useThis} och jag är ${number} år gammal.`;
+
+// console.log(withPlaceholders);
+
+var varName = 'var Joakim';
+let letName = 'let Joakim';
+const constName = 'const Joakim';
+
+// console.log(varName);
+// console.log(letName);
+// console.log(constName);
+
+varName = 'var Jeanette'
+letName = 'let Jeanette'
+// constName = 'const Jeanette'
+
+// console.log(varName);
+// console.log(letName);
+
+let myName = 'Joakim Wahlström';
+
+// console.log(myName.length);
+// console.log(myName[0]);
+// console.log(myName[1]);
+// console.log(myName.indexOf('a'));
+
+// console.log(myName.toUpperCase());
+// console.log(myName.toLowerCase());
+// console.log(myName.toLowerCase().trim());
+
+// console.log(myName.split(' '));
+
+let names = myName.split(' ');
+let firstName = names[0];
+let lastName = names[1];
+console.log(`Mitt förnamn är ${firstName} och efternamnet är ${lastName}`);
+
+let fullName = firstName + ' ' + lastName.toUpperCase();
+console.log(fullName);
+
+let isActive = false;
+console.log(isActive);
