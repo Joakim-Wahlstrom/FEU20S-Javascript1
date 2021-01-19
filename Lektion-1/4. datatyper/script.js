b35829c51e59e134ffec00e103b4e85e3d52e165
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-01-19 11:05:41 +0100

    lektion-1

---
+++ Lektion-1/4. datatyper/script.js
@@ -0,0 +1,35 @@
+/* 
+    Javascript Datatyper
+    ---------------------------------------------
+    String      =   Text
+    Number      =   Tal, decimaltal
+    Boolean     =   Sant/falskt  true / false
+    null        =   null är tomt, inget alls (manuellt)
+    undefined   =   har ej blivit definierat än (automatiskt)
+    Object      =   ett object/klass innehåller ett eller flera värden
+    Array       =   en lista med värden, Arrayer är object
+
+*/
+
+/* 
+    Deklarera variabler
+    ---------------------------------------------
+
+    name = 'Joakim' (ANVÄND INTE!)
+    var name = 'Joakim'
+    let name = 'Joakim'
+    const name = 'Joakim'
+
+    var   = global variabel - accessbar överallt i koden (gör inte såhär)
+    let   = lokal variabel - accessbar bara inom det angivna området (gör såhär)
+    const = lokal konstant variabel, kan inte skrivas över (undantag finns)
+
+    C# exempel:
+      string name = "Joakim";
+      int age = 34;
+    JS exempel:
+      let name = 'Joakim;
+      let age = 34;
+
+
+*/
