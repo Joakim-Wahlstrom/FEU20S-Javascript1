f93a5d67058f3fc74308ee85c7ee44f455bce6fe
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-01-19 13:53:47 +0100

    lektion-1

--- Lektion-1/4. datatyper/script.js
+++ Lektion-1/4. datatyper/script.js
@@ -97,10 +97,92 @@ let myName = 'Joakim Wahlström';
 let names = myName.split(' ');
 let firstName = names[0];
 let lastName = names[1];
-console.log(`Mitt förnamn är ${firstName} och efternamnet är ${lastName}`);
+// console.log(`Mitt förnamn är ${firstName} och efternamnet är ${lastName}`);
 
 let fullName = firstName + ' ' + lastName.toUpperCase();
-console.log(fullName);
+// console.log(fullName);
 
 let isActive = false;
-console.log(isActive);
+// console.log(isActive);
+
+firstName = firstName.concat(' är bäst');
+firstName = firstName.replace('bäst', 'kung')
+// console.log(firstName);
+
+
+
+/* 
+    NUMBERS
+    -------------------------------------
+
+    math operators:
+
+    +   lagg till
+    -   ta bort
+'   *   gångra
+'   **  upphöjt i
+    /   dela
+    %   division remainer
+    ++  öka med 1
+    --  minska med 1
+
+    =   lika med
+    +=  ha kvar det gamla och lägga till
+    -=  ha kvar det gamla och ta bort från
+'   *=  ha kvar det gamla och gångra och lägg till
+    /=  ha kvar det gamla och dela och lägg till
+    %=  ha kvar det gamla och division remainder
+
+*/
+
+let nr1 = 10;
+let nr2 = 2.5;
+
+// console.log(nr1, nr2);
+// console.log(nr1 + nr2);
+// console.log(nr1 - nr2);
+nr1 = nr1 + 1
+nr1 += 1
+nr1 ++
+
+nr1 = nr1 - 1
+nr1 -= 1
+nr1 --
+
+// console.log(nr1);
+
+// gånger
+// console.log(nr1 * nr2);
+// upphöjt i
+// console.log(nr1 ** nr2);
+// vad blir kvar, division remainder
+// console.log(nr1 % 2);
+
+let num = 1000000;
+
+// console.log(num.toFixed(2));
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(Math.random()*100));
+
+let region = 'sv-SE'
+
+// console.log(num.toLocaleString(region));
+
+let datatype;
+
+// let datatype1, datatype2, datatype3;
+
+console.log(typeof datatype);
+datatype = 'en text';
+console.log(typeof datatype);
+datatype = 3;
+console.log(typeof datatype);
+datatype = null; // DET HÄR ÄR EN BUGG I JAVASCRIPT
+console.log(typeof datatype);
+datatype = true;
+console.log(typeof datatype);
+datatype = {};
+console.log(typeof datatype);
+datatype = [];
+console.log(typeof datatype);
