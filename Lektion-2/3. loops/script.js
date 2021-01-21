3e06fafa7f565cda81883dd2307afae2d0efa0b9
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-01-21 13:43:10 +0100

    lektion2

---
+++ Lektion-2/3. loops/script.js
@@ -0,0 +1,11 @@
+const namesArray = ['Joakim', 'Jeanette', 'Jack', 'Colin', 'Vince', 'Liam', 'Wille'];
+
+const usersArray = [
+  {name: 'Joakim', email: 'joakim@mail.com', age: 34, status: true},
+  {name: 'Jeanette', email: 'jeanette@mail.com', age: 33, status: false},
+  {name: 'Jack', email: 'jack@mail.com', age: 5, status: false},
+  {name: 'Colin', email: 'colin@mail.com', age: 3, status: false},
+  {name: 'Vince', email: 'vince@mail.com', age: 1, status: false},
+  {name: 'Liam', email: 'liam@mail.com', age: 15, status: true},
+  {name: 'Wille', email: 'wille@mail.com', age: 9, status: false}
+]
