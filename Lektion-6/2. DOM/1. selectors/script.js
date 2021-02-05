0d25819a8a16e93999238ac771f186f0a4d590b5
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-05 11:39:37 +0100

    lektion-6

---
+++ Lektion-6/2. DOM/1. selectors/script.js
@@ -0,0 +1,15 @@
+/* 
+    DOM - Document Object Model
+*/
+
+// console.log(window)
+
+// Det första jag vill göra är att skapa referenser till mina html element
+let p1 = document.getElementsByTagName('p');
+console.log(p1);
+
+let p2 = document.getElementsByClassName('paragraph');
+console.log(p2);
+
+let p3 = document.getElementById('para');
+console.log(p3);
