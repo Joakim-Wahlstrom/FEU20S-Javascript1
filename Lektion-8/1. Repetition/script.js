49b9cef812967dddca641d05dc84915aa47e15a5
Joakim Wahlström <wahlstrom.joakim@gmail.com>
2021-02-12 09:48:31 +0100

    lektion-8

---
+++ Lektion-8/1. Repetition/script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#form');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+const toc = document.querySelector('#toc');
+
